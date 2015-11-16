683dd7d1c727de9f79ed29720c13e6f29fe599e7
Denis Rizov <d.b.rizov@gmail.com>
2015-11-16 20:03:54 +0200

    Initial commit

---
+++ Assets/Fluid Buoyancy/Scripts/FluidSimulator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluidSimulator : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
