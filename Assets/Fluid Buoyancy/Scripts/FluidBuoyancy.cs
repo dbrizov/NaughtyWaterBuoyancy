30f349d9f402b45554efdf1749589aa7ffb5cc7c
Denis Rizov <d.b.rizov@gmail.com>
2015-11-21 11:41:26 +0200

    Added surface drag

--- Assets/Fluid Buoyancy/Scripts/FluidBuoyancy.cs
+++ Assets/Fluid Buoyancy/Scripts/FluidBuoyancy.cs
@@ -7,7 +7,8 @@ namespace FluidBuoyancy
     [RequireComponent(typeof(BoxCollider))]
     public class FluidBuoyancy : MonoBehaviour
     {
-        private const float CLOSE_VERTEX_RADIUS = 1.5f;
+        private const float CLOSE_VERTEX_THRESHOLD = 2f;
+        private const float SURFACE_RAYCAST_STEP = 0.5f;
 
         [SerializeField]
         private float force = 15f;
@@ -16,6 +17,9 @@ namespace FluidBuoyancy
         [Tooltip("Stickiness")]
         private float viscosity = 2f;
 
+        [SerializeField]
+        private LayerMask ignoreLayer = 0;
+        
         private Mesh mesh;
         private Dictionary<int, AffectedGameObject> affectedGameObjectById;
 
@@ -25,21 +29,59 @@ namespace FluidBuoyancy
             this.affectedGameObjectById = new Dictionary<int, AffectedGameObject>();
         }
 
-        protected virtual void Update()
+        protected virtual void FixedUpdate()
         {
             var force = new Vector3(0f, this.force, 0f);
             var fluidVerticesInWorldSpace = this.mesh.vertices.Select(v => this.transform.localToWorldMatrix.MultiplyPoint3x4(v));
 
             foreach (var affectedGOById in this.affectedGameObjectById)
             {
-                List<Vector3> underwaterVertices = this.GetUnderwaterVerticesInWorldSpace(affectedGOById.Value, fluidVerticesInWorldSpace);
-                if (underwaterVertices.Count > 0f)
+                Vector3 center = affectedGOById.Value.GameObject.transform.localToWorldMatrix.MultiplyPoint3x4(affectedGOById.Value.BoxCollider.center);
+                if (this.IsPointUnderwater(center, fluidVerticesInWorldSpace))
                 {
+                    List<Vector3> underwaterVertices = this.GetUnderwaterVerticesInWorldSpace(affectedGOById.Value, fluidVerticesInWorldSpace);
+                    underwaterVertices.Add(center);
+
+                    foreach (var vert in underwaterVertices)
+                    {
+                        Debug.DrawLine(vert - Vector3.right * 0.05f, vert + Vector3.right * 0.05f, Color.red);
+                        Debug.DrawLine(vert - Vector3.up * 0.05f, vert + Vector3.up * 0.05f, Color.red);
+                    }
+
                     Vector3 midPoint = this.GetMidPoint(underwaterVertices);
+                    Debug.DrawLine(midPoint, midPoint + Vector3.up * 3f, Color.blue);
 
                     Rigidbody rigidbody = affectedGOById.Value.Rigidbody;
-                    rigidbody.AddForceAtPosition(force, midPoint);
-                    rigidbody.AddForceAtPosition(rigidbody.velocity * -1f * this.viscosity, midPoint);
+                    rigidbody.AddForceAtPosition(force, midPoint, ForceMode.Acceleration);
+                    rigidbody.AddForceAtPosition(rigidbody.velocity * -1f * this.viscosity, midPoint, ForceMode.Acceleration);
+
+                    // Add surface drag
+                    float maxDimention = this.GetMaxDimention(affectedGOById.Value);
+                    Vector3 frontPoint = affectedGOById.Value.GameObject.transform.position + (rigidbody.velocity.normalized * maxDimention);
+                    Vector3 planeVector1 = Vector3.Cross(rigidbody.velocity, Vector3.right).normalized;
+                    Vector3 planeVector2 = Vector3.Cross(rigidbody.velocity, planeVector1).normalized;
+
+                    float halfMaxDimention = maxDimention / 2f;
+                    for (float x = -halfMaxDimention; x <= halfMaxDimention; x += SURFACE_RAYCAST_STEP)
+                    {
+                        for (float y = -halfMaxDimention; y <= halfMaxDimention; y += SURFACE_RAYCAST_STEP)
+                        {
+                            Vector3 start = frontPoint + (planeVector1 * x) + (planeVector2 * y);
+                            Ray ray = new Ray(start, -rigidbody.velocity);
+                            RaycastHit hit;
+                            if (Physics.Raycast(ray, out hit, maxDimention * 2f, ~this.ignoreLayer))
+                            {
+                                if (hit.transform.gameObject == affectedGOById.Value.GameObject)
+                                {
+                                    Debug.DrawLine(ray.origin, hit.point, Color.magenta);
+                                    rigidbody.AddForceAtPosition(rigidbody.velocity * -0.05f, hit.point, ForceMode.Acceleration);
+                                }
+                            }
+                        }
+                    }
+
+                    //Debug.DrawLine(center + rigidbody.velocity.normalized + planeVector1, center + rigidbody.velocity.normalized - planeVector1, Color.magenta);
+                    //Debug.DrawLine(center + rigidbody.velocity.normalized + planeVector2, center + rigidbody.velocity.normalized - planeVector2, Color.magenta);
                 }
             }
         }
@@ -73,6 +115,12 @@ namespace FluidBuoyancy
             return false;
         }
 
+        private float GetMaxDimention(AffectedGameObject affectedGO)
+        {
+            var bounds = Vector3.Scale(affectedGO.BoxCollider.size, affectedGO.GameObject.transform.localScale);
+            return Mathf.Max(bounds.x, bounds.y, bounds.z);
+        }
+
         private Vector3[] GetBoxColliderVerticesInWorldSpace(BoxCollider boxCollider)
         {
             Vector3 halfSize = boxCollider.size / 2f;
@@ -100,22 +148,26 @@ namespace FluidBuoyancy
 
             foreach (var vert in verticesInWorldSpace)
             {
-                var closeVerts = fluidVerticesInWorldSpace.Where(
-                    v => Vector3.Distance(new Vector3(v.x, 0f, v.z), new Vector3(vert.x, 0f, vert.z)) <= CLOSE_VERTEX_RADIUS);
-
-                if (closeVerts.Any(v => vert.y < v.y))
+                if (this.IsPointUnderwater(vert, fluidVerticesInWorldSpace))
                 {
                     underwaterVertices.Add(vert);
                 }
             }
 
-            foreach (var vert in underwaterVertices)
+            return underwaterVertices;
+        }
+
+        private bool IsPointUnderwater(Vector3 point, IEnumerable<Vector3> fluidVerticesInWorldSpace)
+        {
+            var closeVerts = fluidVerticesInWorldSpace.Where(
+                    v => Vector3.Distance(new Vector3(v.x, 0f, v.z), new Vector3(point.x, 0f, point.z)) <= CLOSE_VERTEX_THRESHOLD);
+
+            if (closeVerts.Any(v => point.y < v.y))
             {
-                Debug.DrawLine(vert - Vector3.right * 0.05f, vert + Vector3.right * 0.05f, Color.red);
-                Debug.DrawLine(vert - Vector3.up * 0.05f, vert + Vector3.up * 0.05f, Color.red);
+                return true;
             }
 
-            return underwaterVertices;
+            return false;
         }
 
         private Vector3 GetMidPoint(IEnumerable<Vector3> points)
